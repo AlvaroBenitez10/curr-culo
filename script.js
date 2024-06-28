9280938caf8e1cc0830b650d1edce998bdf682f9
AlvaroBenitez10 <benitezalvaro340@gmail.com>
2024-06-28 13:30:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const nombre = ''; 
+
+    const saludo = document.getElementById('saludo');
+    saludo.textContent = `¡Hola, ${nombre}! Bienvenido a mi currículum.`;
+});
